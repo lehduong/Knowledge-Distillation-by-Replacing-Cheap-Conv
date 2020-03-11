5d747d9197138bc7bfd6e9f36cdbdb50480ff7e1
Vo Trung Nhan <1612372@hcmut.edu.vn>
2020-03-11 12:27:15 +0700

    add taylor student, trainer to train.py

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -1,3 +1,4 @@
 from . layerwise_trainer import LayerwiseTrainer
 from .analysis_trainer import AnalysisTrainer
 from .classification_trainer import ClassificationTrainer
+from .taylor_prune_trainer import TaylorPruneTrainer
